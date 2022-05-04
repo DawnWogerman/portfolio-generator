9c30597ce8774e6b8428a339957dad2d7b52a3ff
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-05-04 15:16:49 -0600

    added src folder and page-template app

--- app.js
+++ app.js
@@ -1,58 +1,18 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
 
-const [name, gitHub] = profileDataArgs;
+console.log(profileDataArgs);
 
+const [name, github] = profileDataArgs;
 
-// const generatePage = (userName, gitHubName) => {
-//     return `
-//     Name: ${userName}
-//     GitHub: ${gitHubName}`;
-// };
+console.log(name, github);
 
-const generatePage = (name, gitHub) => {
-    return `
-    <!DOCTYPE html>
-    <html lang ="en"
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-    </head>
-    
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${gitHub}">GitHub</a><h2>
-    </body>
-    </html>
-    `;
-};
-console.log(name,gitHub);
-console.log(generatePage(name, gitHub));
+const pageHTML = generatePage(name, github);
 
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-//9.2.4
-
-
-// const generatePage = () => 'Name: Jane, GitHub: janehub';
-// console.log(generatePage());
-
-// const generatePage = (userName, gitHubName) => `Name: ${userName}, GitHub: ${gitHubName}`;
-// console.log(generatePage('Jane', 'JaneHub'));
-
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
