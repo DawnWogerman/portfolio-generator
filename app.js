13bab2b6935f0ef9b295decd5c10bba5a7f4f91a
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-05-04 18:48:51 -0600

    added new branch and objects

--- app.js
+++ app.js
@@ -1,18 +1,90 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
 
-console.log(profileDataArgs);
+// const fs = require('fs');
 
-const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template');
 
-console.log(name, github);
+// const pageHTML = generatePage(name, github);
 
-const pageHTML = generatePage(name, github);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+const promptUser = () => {
+
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:'
+      }
+    ]);
+  };
+
+  const promptProject = portfolioData => {
+   //if there's no projects array property, create one
+   if(!portfolioData.projects){
+       portfolioData.projects = [];
+   }
+    console.log(`
+    =================
+    Add a New Project
+    ================= 
+    `);
+    
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of your project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to add another project?',
+            default: false
+        }
+    ]);
+  };
+
+
+
+  promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
