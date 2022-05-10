3f56fc7c9ef3c361c41a6f233a61e62dc665880e
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-05-09 18:38:17 -0600

    start of entering info on html template

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generatePage = require('./src/page-template');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
@@ -148,7 +148,13 @@ const promptUser = () => {
 
 
   promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-      console.log(portfolioData);
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
     });
+  });
